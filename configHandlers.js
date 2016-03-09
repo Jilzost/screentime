a0a21216a284d5bef79c1b7e24dfe01b3517de97
Jilzost <davidbarker@Sharpie.home>
2016-03-08 22:17:20 -0500

    Moved sign configuration into master configuration

--- configHandlers.js
+++ configHandlers.js
@@ -7,8 +7,9 @@ var path    = require('path');
 var mime    = require('mime');
 var logger  = require('./logger');
 var _und    = require('underscore');
+var config = require('config');
 var cache   = {};
-var configs;
+//var configs;
 
 /**
  * return a 500 - internal server error.
@@ -30,49 +31,20 @@ function send404(response) {
     response.end();
 }
 
-function buildAndSendResponse(path, id, response) {
-    var config;
-    if (!configs.signConfigs[id]) {
+function getSignConfig(path, id, response) {
+    var vals;
+    if (!config.signConfigs[id]) {
         send404(response);
     } else {
-        if (configs.signConfigs[id].base) {
+        if (config.signConfigs[id].base) {
 
-            config = _und.defaults(configs.signConfigs[id].values, configs.signConfigs.bases[configs.signConfigs[id].base]);
+            vals = _und.defaults(config.signConfigs[id].values, config.signConfigs.bases[config.signConfigs[id].base]);
         } else {
-            config = configs.signConfigs[id].values;
+            vals = config.signConfigs[id].values;
         }
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(JSON.stringify(config));
+        response.write(JSON.stringify(vals));
         response.end();
-    }
-}
-
-function getSignConfig(path, id, response) {
-    logger.log('server', id, 5, 'configHandlers.checkForConfigs',
-        'Request for path ' + path + ' id ' + id);
-    if (!configs) {
-        fs.exists('./configs.json', function (exists) {
-            if (exists) {
-                fs.readFile('./configs.json', function (err, data) {
-                    if (err) {
-                        logger.log('server', id, 1,
-                            'configHandlers.checkForConfigs',
-                            'Error loading configs');
-                        send500(response);
-                    } else {
-                        configs = JSON.parse(data);
-                        buildAndSendResponse(path, id, response);
-                    }
-                });
-            } else {
-                logger.log('server', id, 1,
-                    'configHandlers.checkForConfigs',
-                    'Could not find configs');
-                send500(response);
-            }
-        });
-    } else {
-        buildAndSendResponse(path, id, response);
     }
 }
 
