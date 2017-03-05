1a8c56c8a040721feb073d6af7e453ab5dafd657
Jilzost <davidbarker@Sharpie.home>
2017-03-05 10:55:44 -0500

    Config value logging

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var logger = require("./logger");
 var sampleHandlers = require("./sampleHandlers");
 var speechSampleHandlers = require("./speechSampleHandlers");
 var socketServer = require("./socketServer");
+var config = require('config');
 
 
 var handle = {};
@@ -41,4 +42,9 @@ handle["/speak.mp3"] = socketServer.speak;
 
 server.start(router.route, handle);
 logger.startLogging();
-
+logger.log('server', 'server', 4, 'server.start', 'NODE_ENV: ' + config.util.getEnv('NODE_ENV'));
+logger.log('server', 'server', 4, 'server.start', 'NODE_CONFIG_DIR: ' + config.util.getEnv('NODE_CONFIG_DIR'));
+logger.log('server', 'server', 4, 'server.start', 'NODE_CONFIG: ' + config.util.getEnv('NODE_CONFIG'));
+logger.log('server', 'server', 4, 'server.start', 'HOSTNAME: ' + config.util.getEnv('HOSTNAME'));
+logger.log('server', 'server', 4, 'server.start', 'NODE_APP_INSTANCE: ' + config.util.getEnv('NODE_APP_INSTANCE'));
+logger.log('server', 'server', 4, 'server.start', 'NODE_ENV: ' + config.util.getEnv('NODE_ENV'));
