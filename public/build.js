9aee60605a9e8baf9080dc1a781f14064fadbb33
Jilzost <davidbarker@Sharpie.home>
2017-07-30 19:47:30 -0400

    requirejs optimized and uglified

---
+++ public/build.js
@@ -0,0 +1,12 @@
+({
+  baseUrl: "./js",
+  paths: {
+    "jquery": "libs/jquery",
+    "underscore": "libs/underscore",
+    "backbone": "libs/backbone",
+    "mespeak": "libs/mespeak/mespeak.full",
+    "io": "libs/socket.io"
+  },
+  name: "main",
+  out: "main-built.js"
+})
